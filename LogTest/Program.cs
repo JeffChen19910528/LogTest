a190cd651fcff57c5f9252640842058fb456f7a3
Hsinchueh Chen 陳新爵 <1709002@iisigroup.com>
2020-07-24 12:00:55 +0800

    加入專案檔案。

---
+++ LogTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using NLog;
+using System;
+
+namespace LogTest
+{
+    class Program
+    {
+        private static Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            try
+            {
+                // log here
+                logger.Trace("Trace");
+                logger.Debug("Debug");
+                logger.Info("Info");
+                logger.Warn("Warn");
+                logger.Error("Error");
+                logger.Fatal("Fatal");
+
+                int div = 0;
+                div /= div;     //故意讓系統出現例外
+
+            }
+            catch (Exception ex)
+            {
+                // log with exception here
+                logger.Trace(ex, "Trace");
+                logger.Debug(ex, "Debug");
+                logger.Info(ex, "Info");
+                logger.Warn(ex, "Warn");
+                logger.Error(ex, "Error");
+                logger.Fatal(ex, "Fatal");
+
+            }
+        }
+    }
+}
